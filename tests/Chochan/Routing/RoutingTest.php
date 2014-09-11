783173cd2fbef718499d41913d8efae18ef46e35
ata <ata@debian.ata.com.ar>
2014-09-10 23:49:15 -0300

    Adding routing test

---
+++ tests/Chochan/Routing/RoutingTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+* This file is part of Chochan framework
+* 
+* @author    Gabriel Alonso <gbr.alonso@gmail.com>
+* @copyright 2014
+* @license   WTFPL - http://www.wtfpl.net/txt/copying/
+* 
+*/
+
+namespace Chochan\Routing;
+
+require realpath(__DIR__.'/../../../src/Chochan/').'/autoload.php';
+
+/**
+ * Routing class test
+ * 
+*/
+class RoutingTest extends \PHPUnit_Framework_TestCase
+{
+    protected $request;
+
+    protected function setUp()
+    {
+        parent::setUp();
+    
+        $this->router = new Router();
+    }
+
+    public function testRouting()
+    {
+        $this->router->setBaseDir("/");
+
+        $this->router->register("greeting/:name/:lastName?", function ($name, $lastName = "") {
+            return "Hello $name $lastName!!!";
+        });
+
+        $routemap = $this->router->match("GET", "/greeting/chochan/framework");
+
+        $this->assertEquals("Hello chochan framework!!!", call_user_func_array($routemap[0], array_values($routemap[1])));
+        
+    }
+}
